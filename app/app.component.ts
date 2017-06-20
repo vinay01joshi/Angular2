c7a64dd6827198e086dcce9d251c5fba2cafa06c
Vinay Joshi <vinay01joshi@gmail.com>
2017-06-20 19:55:37 +0530

    Adding first component angular2

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,9 @@
+import { CoursesComponent } from './courses.component';
 import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>My First Angular 2 App</h1><courses></courses>',
+    directives:[CoursesComponent]
 })
 export class AppComponent { }
