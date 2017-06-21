2484fd94a85b04ddd8af7d86a92104a024650785
Your Name <you@example.com>
2017-06-21 15:47:13 +0530

    moving template in separate file using template url

--- app/star.component.ts
+++ app/star.component.ts
@@ -2,15 +2,10 @@ import { Component, Input, EventEmitter, Output } from "angular2/core";
 
 @Component({
     selector : 'star',
-    template : ` <i class="glyphicon"
-                    [class.glyphicon-star-empty]="!isFavorite"
-                    [class.glyphicon-star]="isFavorite"
-                    (click) = "onClick()">
-                 </i>
-                `
+    templateUrl : 'app/star.templage.html'
 })
 export class StarComponent {
-    @Input('is-favorite') isFavorite = false;// making this property as public
+    @Input('is-favorite') isFavorite = false;// making this property as public comment
 
     @Output() change = new EventEmitter();
 
