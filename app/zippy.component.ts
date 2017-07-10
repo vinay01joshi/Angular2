9db75810cdefb2682d6a7d385abc72930f4f8d91
Vinay Joshi <vinay01joshi@gmail.com>
2017-07-10 16:43:42 +0530

    Some style changes in zippy component

--- app/zippy.component.ts
+++ app/zippy.component.ts
@@ -2,26 +2,46 @@ import { Component, Input } from "angular2/core";
 
 @Component({
     selector : 'zippy',
-    template : `
+    styles: [`
+        .zippy {
+            border: 1px solid #ccc;
+            border-radius: 2px;
+        }
+        
+        .zippy .zippy-title {
+            padding: 20px;
+            font-weight: bold;
+        }
+        
+        .zippy .zippy-title:hover{
+            background: #f0f0f0;
+            cursor: pointer;
+        }
+        
+        .zippy .zippy-content {
+            padding: 20px;
+        }
+    `],
+  template: `
     <div class="zippy">
         <div 
             class="zippy-title"
             (click)="toggle()">
-            {{title}}
-            <i
-                class="pull-right glyphicon"
+            {{ title }} 
+            <i 
+                class="pull-right glyphicon" 
                 [ngClass]="
-                    {
-                        'glyphicon-chevron-down' : !isExpanded,
-                        'glyphicon=chevron-up' : isExpanded
+                    { 
+                        'glyphicon-chevron-down': !isExpanded, 
+                        'glyphicon-chevron-up': isExpanded 
                     }">
             </i>
         </div>
         <div *ngIf="isExpanded" class="zippy-content">
-            <ng-content></ng-content>
+            <ng-content></ng-content> 
         </div>
-    </div>
-    `
+    </div>    
+    `    
 })
 export class ZippyComponent {
     isExpanded = false ;  
