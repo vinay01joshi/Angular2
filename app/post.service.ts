96c027b34ff07960e94a39661971d6a5e7756b1b
Vinay Joshi <vinay01joshi@gmail.com>
2017-07-13 19:17:03 +0530

    Adding some more code for static type binding which was mssing

--- app/post.service.ts
+++ app/post.service.ts
@@ -1,6 +1,8 @@
 import {Http} from 'angular2/http';
+import { Observable } from "rxjs/Observable";
 import 'rxjs/add/operator/map';
 import {Injectable} from 'angular2/core';
+import {Post} from './post';
 
 @Injectable()
 export class PostService {
@@ -9,12 +11,12 @@ export class PostService {
 
     }
 
-    getPosts(){
+    getPosts() : Observable<Post[]> {
       return  this._http.get(this._url)
            .map(res=> res.json());        
     }
 
-    cratePost(post){
+    cratePost(post : Post){
         return this._http.post(this._url,JSON.stringify(post))
             .map(res => res.json())
     }
