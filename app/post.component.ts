5be00acab4956b3806247cd304f98d07bd90dee2
Vinay Joshi <vinay01joshi@gmail.com>
2017-07-13 19:05:45 +0530

    :Wrench: adding loading icon using font awsome

--- app/post.component.ts
+++ app/post.component.ts
@@ -6,17 +6,25 @@ import {OnInit} from 'angular2/core';
 @Component({
     selector : 'post-component',
     template : `
+        <div *ngIf="isLoading">
+            <i class="fa fa-spinner fa-spin fa-3x"></i>
+        </div>
     `,
     providers :[PostService,HTTP_PROVIDERS]
 })
 export class PostComponent implements OnInit {
+    isLoading = true;
+
     constructor(private _postService: PostService) {
        // this._postService.cratePost({userId : 1, title : "a" , body: "b"});
     }
 
     ngOnInit() {
         this._postService.getPosts()
-            .subscribe(posts => console.log(posts));
+            .subscribe(posts => {
+                this.isLoading = false;
+                console.log(posts)
+            });
     }
     
 }
